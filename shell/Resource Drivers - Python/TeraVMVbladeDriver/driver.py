4eb165c1e232da2632ef1cb9ab2e4b6f8037b849
Anthony Piddubny <anton.p@qualisystems.com>
2018-02-20 18:00:45 +0200

    add connect_child_resources command

--- shell/Resource Drivers - Python/TeraVMVbladeDriver/driver.py
+++ shell/Resource Drivers - Python/TeraVMVbladeDriver/driver.py
@@ -1,5 +1,7 @@
+import copy
 import json
 
+from cloudshell.api.cloudshell_api import SetConnectorRequest
 from cloudshell.core.context.error_handling_context import ErrorHandlingContext
 from cloudshell.devices.driver_helper import get_api
 from cloudshell.devices.autoload.autoload_builder import AutoloadDetailsBuilder
@@ -13,6 +15,10 @@ from cloudshell.traffic.teravm.vblade.configuration_attributes_structure import 
 from cloudshell.traffic.teravm.vblade.autoload import models
 
 
+
+ATTR_REQUESTED_SOURCE_VNIC = "Requested Source vNIC Name"
+ATTR_REQUESTED_TARGET_VNIC = "Requested Target vNIC Name"
+MODEL_PORT = "TeraVM Virtual Traffic Generator Port"
 VCENTER_RESOURCE_USER_ATTR = "User"
 VCENTER_RESOURCE_PASSWORD_ATTR = "Password"
 
@@ -124,6 +130,123 @@ class TeraVMVbladeDriver(ResourceDriverInterface):
 
         pass
 
+    def connect_child_resources(self, context):
+        """
+
+        :type context: cloudshell.shell.core.driver_context.ResourceCommandContext
+        :rtype: str
+        """
+        logger = get_logger_with_thread_id(context)
+        logger.info("Connect child resources command started")
+
+        with ErrorHandlingContext(logger):
+            api = get_api(context)
+
+            resource_name = context.resource.fullname
+            reservation_id = context.reservation.reservation_id
+            connectors = context.connectors
+
+            if not context.connectors:
+                return "Success"
+
+            resource = api.GetResourceDetails(resource_name)
+
+            to_disconnect = []
+            to_connect = []
+            temp_connectors = []
+            ports = self._get_ports(resource)
+
+            for connector in connectors:
+                me, other = self._set_remap_connector_details(connector, resource_name, temp_connectors)
+                to_disconnect.extend([me, other])
+
+            connectors = temp_connectors
+
+            # these are connectors from app to vlan where user marked to which interface the connector should be connected
+            connectors_with_predefined_target = [connector for connector in connectors if connector.vnic_id != ""]
+
+            # these are connectors from app to vlan where user left the target interface unspecified
+            connectors_without_target = [connector for connector in connectors if connector.vnic_id == ""]
+
+            for connector in connectors_with_predefined_target:
+                if connector.vnic_id not in ports.keys():
+                    raise Exception("Tried to connect an interface that is not on reservation - " + connector.vnic_id)
+
+                else:
+                    if hasattr(ports[connector.vnic_id], "allocated"):
+                        raise Exception(
+                            "Tried to connect several connections to same interface: " + ports[connector.vnic_id])
+
+                    else:
+                        to_connect.append(SetConnectorRequest(SourceResourceFullName=ports[connector.vnic_id].Name,
+                                                              TargetResourceFullName=connector.other,
+                                                              Direction=connector.direction,
+                                                              Alias=connector.alias))
+                        ports[connector.vnic_id].allocated = True
+
+            unallocated_ports = [port for key, port in ports.items() if not hasattr(port, "allocated")]
+
+            if len(unallocated_ports) < len(connectors_without_target):
+                raise Exception("There were more connections to TeraVM than available interfaces after deployment.")
+            else:
+                for port in unallocated_ports:
+                    if connectors_without_target:
+                        connector = connectors_without_target.pop()
+                        to_connect.append(SetConnectorRequest(SourceResourceFullName=port.Name,
+                                                              TargetResourceFullName=connector.other,
+                                                              Direction=connector.direction,
+                                                              Alias=connector.alias))
+
+            if connectors_without_target:
+                raise Exception("There were more connections to TeraVM than available interfaces after deployment.")
+
+            api.RemoveConnectorsFromReservation(reservation_id, to_disconnect)
+            api.SetConnectorsInReservation(reservation_id, to_connect)
+
+            return "Success"
+
+    @staticmethod
+    def _set_remap_connector_details(connector, resource_name, connectors):
+        attribs = connector.attributes
+        if resource_name in connector.source.split("/"):
+            remap_requests = attribs.get(ATTR_REQUESTED_SOURCE_VNIC, "").split(",")
+
+            me = connector.source
+            other = connector.target
+
+            for vnic_id in remap_requests:
+                new_con = copy.deepcopy(connector)
+                TeraVMVbladeDriver._update_connector(new_con, me, other, vnic_id)
+                connectors.append(new_con)
+
+        elif resource_name in connector.target.split("/"):
+            remap_requests = attribs.get(ATTR_REQUESTED_TARGET_VNIC, "").split(",")
+
+            me = connector.target
+            other = connector.source
+
+            for vnic_id in remap_requests:
+                new_con = copy.deepcopy(connector)
+                TeraVMVbladeDriver._update_connector(new_con, me, other, vnic_id)
+                connectors.append(new_con)
+        else:
+            raise Exception("Oops, a connector doesn't have required details:\n Connector source: {0}\n"
+                            "Connector target: {1}\nPlease contact your admin".format(connector.source,
+                                                                                      connector.target))
+
+        return me, other
+
+    @staticmethod
+    def _update_connector(connector, me, other, vnic_id):
+        connector.vnic_id = vnic_id
+        connector.me = me
+        connector.other = other
+
+    @staticmethod
+    def _get_ports(resource):
+        ports = {str(idx): port for idx, port in enumerate(resource.ChildResources)
+                 if port.ResourceModelName == MODEL_PORT}
+        return ports
 
 if __name__ == "__main__":
     import mock
